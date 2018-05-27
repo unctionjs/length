f3eed5241ca654ec40ac731d8e183b30b063eeb3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import values from "@unction/values"
-
-export default function length (functor: ArrayType & ObjectType & MapType & SetType & string): number {
-  return functor.length || functor.size || values(functor).length
+import values from "@unction/values";
+export default function length(functor) {
+  return functor.length || functor.size || values(functor).length;
 }
