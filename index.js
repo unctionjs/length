1cc03418330df089414c14da3613471eb1f6858b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:44 -0700

    Autolinting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import values from "@unction/values";
-export default function length(functor) {
+export default function length (functor) {
   return functor.length || functor.size || values(functor).length;
 }
--- test.js
+++ test.js
@@ -1,37 +1,37 @@
 /* eslint-disable flowtype/require-return-type */
-import {test} from "tap"
+import {test} from "tap";
 
-import length from "./index"
+import length from "./index";
 
 test(({same, end}) => {
   same(
     length([1, 2, 3]),
     3
-  )
+  );
 
-  end()
-})
+  end();
+});
 test(({same, end}) => {
   same(
     length({aaa: "aaa", bbb: "bbb"}),
     2
-  )
+  );
 
-  end()
-})
+  end();
+});
 test(({same, end}) => {
   same(
     length(new Map([["aaa", "aaa"], ["bbb", "bbb"]])),
     2
-  )
+  );
 
-  end()
-})
+  end();
+});
 test(({same, end}) => {
   same(
     length(new Set([1, 2, 3])),
     3
-  )
+  );
 
-  end()
-})
+  end();
+});
