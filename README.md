e0beb4be685f0fdfb0ba9c09133fb91400f9ae55
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:50 -0800

    Removing more bad types

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> Array<A> | Set<A> | RecordType<unknown, A> | string => number
+> Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => number
 
 Returns the number of values contained in the enumerable.
 
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import values from "@unction/values";
 
-export default function length<A> (enumerable: Array<A> | Set<A> | RecordType<unknown, A> | string): number {
+export default function length<A> (enumerable: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): number {
   return enumerable.length || enumerable.size || values(enumerable).length;
 }
